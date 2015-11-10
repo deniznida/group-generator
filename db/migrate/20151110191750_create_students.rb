040a53f2d36836e469dc7296a9cef0fa100ad6b1
deniznida <denizunat@hotmail.com>
2015-11-10 14:33:00 -0500

    correct migrations

--- db/migrate/20151110191750_create_students.rb
+++ db/migrate/20151110191750_create_students.rb
@@ -1,7 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-
+      t.string :name
+      t.string :email
+      t.integer :cohort_id
       t.timestamps null: false
     end
   end
--- db/migrate/20151110191836_create_student_groups.rb
+++ db/migrate/20151110191836_create_student_groups.rb
@@ -1,9 +1,8 @@
 class CreateStudentGroups < ActiveRecord::Migration
   def change
     create_table :student_groups do |t|
-      t.string :name
-      t.string :email
-      t.integer :cohort_id
+      t.integer :group_id
+      t.integer :student_id
       t.timestamps null: false
     end
   end
