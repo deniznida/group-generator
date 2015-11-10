0c800a767c3471ec40e7c493de6422d8e136c707
deniznida <denizunat@hotmail.com>
2015-11-10 16:01:43 -0500

    change migration

--- db/migrate/20151110191812_create_groups.rb
+++ db/migrate/20151110191812_create_groups.rb
@@ -1,7 +1,6 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
-      t.string :name
       t.integer :group_type_id
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20151110191836) do
   end
 
   create_table "groups", force: :cascade do |t|
-    t.string   "name"
     t.integer  "group_type_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
