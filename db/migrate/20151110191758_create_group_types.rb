474036e3ce7fa12cfc772e52be19d2dbaf7a0374
makafis <vinnyalfieri@gmail.com>
2015-11-11 17:20:07 -0500

    added seed data and fixed type column name since type is reserved

--- db/migrate/20151110191758_create_group_types.rb
+++ db/migrate/20151110191758_create_group_types.rb
@@ -1,7 +1,7 @@
 class CreateGroupTypes < ActiveRecord::Migration
   def change
     create_table :group_types do |t|
-      t.string :type
+      t.string :name
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20151110191836) do
   end
 
   create_table "group_types", force: :cascade do |t|
-    t.string   "type"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
