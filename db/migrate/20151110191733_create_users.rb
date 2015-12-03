7221de8f9fb86302299d4116273f30b66610297a
deniznida <denizunat@hotmail.com>
2015-12-03 15:16:38 -0500

    add github column to user

--- db/migrate/20151110191733_create_users.rb
+++ db/migrate/20151110191733_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :github_username
+      
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,9 @@ ActiveRecord::Schema.define(version: 20151110191836) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "github_username"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
