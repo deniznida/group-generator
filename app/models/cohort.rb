1ad48b40fc31b9dc1dbc0b5a81338235e8bfb34a
deniznida <denizunat@hotmail.com>
2015-12-04 13:54:50 -0500

    find or init from csv

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -10,22 +10,13 @@ class Cohort < ActiveRecord::Base
 
   def create_or_update_user_from_csv(file)
     CSV.foreach(file.path, headers: true) do |row|
-      @student = Student.find_or_create_by(github_username: row["github_username"]) do |student|
-        student.cohort = self
-        student.name = "#{row["first_name"]} #{row["last_name"]}"
-        student.email = row["email"]
-        student.save
-      end
-    end
+    
+      student = Student.where(github_username: row["github_username"]).first_or_initialize
+      
+      name = "#{row["first_name"]} #{row["last_name"]}"
+      email = row["email"]
+      
+      student.update(name: name, email: email, cohort_id: self.id)
+    end  
   end
-
-  # def create_or_update_user_from_csv(row)
-  #   @member = User.where(github_username: row[@github_username_index]).first_or_initialize.tap do |user|
-  #     @member_attributes_array.compact.each_with_index do |attribute, index|
-  #       user.send(attribute + '=', row[index])
-  #     end
-  #     user.save
-  #   end
-  # end
-
 end
